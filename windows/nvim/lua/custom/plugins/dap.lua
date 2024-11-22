4e345ead98ddd1780bcc3840872c4dca8ebd8f06
williycole <64283810+williycole@users.noreply.github.com>
2024-11-22 08:42:53 -0600

    more dap / dev container tweaks

--- windows/nvim/lua/custom/plugins/dap.lua
+++ windows/nvim/lua/custom/plugins/dap.lua
@@ -1,106 +1,128 @@
--- return {
---   {
---     'mfussenegger/nvim-dap',
---     dependencies = {
---       'rcarriga/nvim-dap-ui',
---       'williamboman/mason.nvim',
---       'jay-babu/mason-nvim-dap.nvim',
---       'leoluz/nvim-dap-go',
---     },
---     config = function()
---       local dap = require 'dap'
---       local dapui = require 'dapui'
---
---       require('mason-nvim-dap').setup {
---         automatic_setup = true,
---         handlers = {},
---         ensure_installed = {
---           'delve',
---         },
---       }
---
---       -- Keymaps
---       vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
---       vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
---       vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
---       vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
---       vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
---       vim.keymap.set('n', '<leader>B', function()
---         dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
---       end, { desc = 'Debug: Set Breakpoint' })
---
---       -- DAP UI setup
---       dapui.setup()
---       vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: Toggle UI' })
---
---       dap.listeners.after.event_initialized['dapui_config'] = dapui.open
---       dap.listeners.before.event_terminated['dapui_config'] = dapui.close
---       dap.listeners.before.event_exited['dapui_config'] = dapui.close
---
---       -- Go debugger setup
---       require('dap-go').setup()
---
---       -- Configure Go debugger
---       dap.configurations.go = {
---         {
---           type = 'go',
---           name = 'Debug',
---           request = 'attach',
---           mode = 'remote',
---           remotePath = '/src',
---           port = 2345,
---           host = '127.0.0.1',
---           cwd = '${workspaceFolder}',
---         },
---       }
---     end,
---   },
--- }
---
 return {
+  -- Dap
   {
     'mfussenegger/nvim-dap',
     dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'williamboman/mason.nvim',
+      'jay-babu/mason-nvim-dap.nvim',
       'leoluz/nvim-dap-go',
     },
     config = function()
       local dap = require 'dap'
-      local dap_go = require 'dap-go'
+      local dapui = require 'dapui'
 
-      dap_go.setup()
-
-      table.insert(dap.configurations.go, {
-        type = 'go',
-        name = 'Debug in Docker',
-        request = 'attach',
-        mode = 'remote',
-        remotePath = '/src',
-        port = 2345,
-        host = '127.0.0.1',
-        cwd = '${workspaceFolder}',
-        substitutePath = {
-          {
-            from = '${workspaceFolder}',
-            to = '/src',
-          },
+      require('mason-nvim-dap').setup {
+        automatic_setup = true,
+        handlers = {},
+        ensure_installed = {
+          'delve',
         },
-      })
+      }
 
-      vim.keymap.set('n', '<F5>', function()
-        require('dap').continue()
-      end)
-      vim.keymap.set('n', '<F10>', function()
-        require('dap').step_over()
-      end)
-      vim.keymap.set('n', '<F11>', function()
-        require('dap').step_into()
-      end)
-      vim.keymap.set('n', '<F12>', function()
-        require('dap').step_out()
-      end)
-      vim.keymap.set('n', '<Leader>b', function()
-        require('dap').toggle_breakpoint()
-      end)
+      -- Keymaps
+      vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
+      vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
+      vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
+      vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+      vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+      vim.keymap.set('n', '<leader>B', function()
+        dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+      end, { desc = 'Debug: Set Breakpoint' })
+
+      -- DAP UI setup
+      dapui.setup()
+      vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: Toggle UI' })
+
+      dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+      dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+      dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+      -- Go debugger setup
+      require('dap-go').setup()
+      -- require('dap').set_log_level 'INFO'
+      require('dap').set_log_level 'TRACE'
+
+      -- Configure Go adapters
+      dap.adapters.go = {
+        type = 'executable',
+        command = 'node',
+        args = { os.getenv 'HOME' .. '/.local/share/nvim/mason/packages/go-debug-adapter/extension/dist/debugAdapter.js' },
+      }
+
+      -- Configure Go debugger
+      dap.configurations.go = {
+        {
+          type = 'go',
+          name = 'Debug : filerDirname',
+          request = 'launch',
+          program = '${fileDirname}',
+        },
+        {
+          type = 'go',
+          name = 'Debug',
+          request = 'attach',
+          mode = 'remote',
+          remotePath = '/src',
+          port = 2345,
+          host = '127.0.0.1',
+          cwd = '${workspaceFolder}',
+        },
+      }
+      -- Configure debugger for unknown files
+      dap.configurations._ = {
+        {
+          type = 'go',
+          name = 'Debug',
+          request = 'attach',
+          mode = 'remote',
+          remotePath = '/src',
+          port = 2345,
+          host = '127.0.0.1',
+          cwd = '${workspaceFolder}',
+        },
+      }
+
+      -- Dap Select Config like vscode
+      local dap_config_selector = function()
+        local configs = dap.configurations.go
+        local names = {}
+        for _, config in ipairs(configs) do
+          table.insert(names, config.name)
+        end
+        vim.ui.select(names, { prompt = 'Select debug configuration:' }, function(choice)
+          if choice then
+            for _, config in ipairs(configs) do
+              if config.name == choice then
+                dap.run(config)
+                break
+              end
+            end
+          end
+        end)
+      end
+
+      vim.keymap.set('n', '<leader>dc', dap_config_selector, { desc = 'Debug: Select Configuration' })
+    end,
+  },
+  -- Tasks
+  {
+    'stevearc/overseer.nvim',
+    opts = {},
+    config = function()
+      require('overseer').setup()
+      vim.api.nvim_create_user_command('OverseerRestartLast', function()
+        local overseer = require 'overseer'
+        local task = overseer.get_last_task()
+        if task then
+          task:restart()
+        else
+          vim.notify('No last task found', vim.log.levels.WARN)
+        end
+      end, {})
+
+      vim.keymap.set('n', '<leader>or', ':OverseerRun<CR>', { desc = 'Run Overseer Task' })
+      vim.keymap.set('n', '<leader>ot', ':OverseerToggle<CR>', { desc = 'Toggle Overseer' })
     end,
   },
 }
--- windows/nvim/lua/custom/plugins/nvim-dev-container.lua
+++ windows/nvim/lua/custom/plugins/nvim-dev-container.lua
@@ -1,55 +1,61 @@
--- default setup
--- see for more options: https://github.com/esensar/nvim-dev-container
--- return {
---   {
---     'https://codeberg.org/esensar/nvim-dev-container',
---     dependencies = 'nvim-treesitter/nvim-treesitter',
---     config = function()
---       require("devcontainer").setup({
---         container_id = "46773be39f16",
---         container_runtime = "docker",
---         container_user = "dev",
---         command_prefix = "sudo",
---         attach_mounts = {
---           neovim_config = {
---             source = vim.fn.expand("~/.config/nvim"),
---             target = "/home/dev/.config/nvim"
---           },
---         },
---         terminal_handler = function(command)
---           if type(command) == "table" then
---             command = table.concat(command, " ")
---           end
---           vim.cmd('vsplit term://' .. command)
---         end,
---       })
---     end
---   }
--- }
--- TODO: Working ish below, commented out bc I'm trying to just attach it to the dev containter with a script
+-- Docs: https://github.com/esensar/nvim-dev-container
 return {
   {
     'https://codeberg.org/esensar/nvim-dev-container',
     dependencies = 'nvim-treesitter/nvim-treesitter',
     config = function()
       require('devcontainer').setup {
-        -- for better consistency we use container name
-        -- container_id = "46773be39f16",
-        container_name = 'ggs-dev-q',
+        container_name = 'ggs-dev-1',
         container_runtime = 'docker',
         container_user = 'dev',
         command_prefix = 'sudo',
         attach_mounts = {
           neovim_config = {
-            source = vim.fn.expand '~/.config/nvim',
-            target = '/home/dev/.config/nvim',
+            -- enables mounting local config to /root/.config/nvim in container
+            enabled = false,
+            -- makes mount readonly in container
+            options = { 'readonly' },
+          },
+          neovim_data = {
+            -- enables mounting local data to /root/.local/share/nvim in container
+            enabled = false,
+            -- no options by default
+            options = {},
+          },
+          -- Only useful if using neovim 0.8.0+
+          neovim_state = {
+            -- enables mounting local state to /root/.local/state/nvim in container
+            enabled = false,
+            -- no options by default
+            options = {},
           },
         },
+        -- terminal_handler = function(command)
+        --   if type(command) == 'table' then
+        --     command = table.concat(command, ' ')
+        --   end
+        --   vim.cmd('tabnew | terminal ' .. command)
+        -- end,
+        -- NOTE: new stuf below here
         terminal_handler = function(command)
-          if type(command) == 'table' then
-            command = table.concat(command, ' ')
-          end
-          vim.cmd('tabnew | terminal ' .. command)
+          require('toggleterm').exec(command)
+        end,
+        -- Add container-specific LSP settings
+        on_attach = function(bufnr)
+          local lspconfig = require 'lspconfig'
+          lspconfig.gopls.setup {
+            cmd = { 'gopls', 'serve' },
+            filetypes = { 'go', 'gomod' },
+            root_dir = lspconfig.util.root_pattern('go.work', 'go.mod', '.git'),
+            settings = {
+              gopls = {
+                analyses = {
+                  unusedparams = true,
+                },
+                staticcheck = true,
+              },
+            },
+          }
         end,
       }
     end,
