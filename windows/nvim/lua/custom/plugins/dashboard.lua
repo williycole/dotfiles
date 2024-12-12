b31d66e13f5e9af1ac3c5cdf440131fba2ef2362
williycole <64283810+williycole@users.noreply.github.com>
2024-12-12 08:51:26 -0600

    added dashboard.nvim

---
+++ windows/nvim/lua/custom/plugins/dashboard.lua
@@ -0,0 +1,50 @@
+-- docs: https://github.com/nvimdev/dashboard-nvim, alternative https://github.com/goolord/alpha-nvim for even more customization
+return {
+  'nvimdev/dashboard-nvim',
+  event = 'VimEnter',
+  opts = {
+    theme = 'hyper',
+    config = {
+      header = {
+        '██╗      ██████╗  ██████╗     ██████╗ ███████╗███████╗',
+        '██║     ██╔═══██╗██╔════╝    ██╔═══██╗██╔════╝██╔════╝',
+        '██║     ██║   ██║██║  ███╗   ██║   ██║█████╗  █████╗  ',
+        '██║     ██║   ██║██║   ██║   ██║   ██║██╔══╝  ██╔══╝  ',
+        '███████╗╚██████╔╝╚██████╔╝   ╚██████╔╝██║     ██║     ',
+        '╚══════╝ ╚═════╝  ╚═════╝     ╚═════╝ ╚═╝     ╚═╝     ',
+      },
+      week_header = {
+        enable = true,
+        concat = true,
+        format = '%Y-%m-%d %H:%M:%S',
+      },
+      shortcut = {
+        { desc = ' Update', group = '@property', action = 'Lazy update', key = 'u' },
+        { desc = ' Files', group = '@property', action = 'Telescope find_files', key = 'f' },
+        { desc = ' Apps', group = '@property', action = 'e ~/.config/nvim/lua/plugins/', key = 'a' },
+        { desc = ' Dotfiles', group = '@property', action = 'e ~/.config/nvim/', key = 'd' },
+      },
+      project = {
+        enable = true,
+        limit = 8,
+        icon = '',
+        label = ' Recently Projects:',
+        action = 'Telescope find_files cwd=',
+      },
+      mru = {
+        limit = 10,
+        icon = '',
+        label = ' Most Recent Files:',
+        cwd_only = false,
+      },
+      footer = {
+        '',
+        'neovim loaded XX packages',
+        '🚀 Sharp tools make good work.',
+      },
+    },
+  },
+  dependencies = {
+    { 'nvim-tree/nvim-web-devicons' },
+  },
+}
