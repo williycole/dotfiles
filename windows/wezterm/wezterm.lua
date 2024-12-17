b44c89164669a3a587222895f8869e5ee81e15ab
unknown <64283810+williycole@users.noreply.github.com>
2024-12-17 15:55:35 -0600

    better wezterm config

--- windows/wezterm/wezterm.lua
+++ windows/wezterm/wezterm.lua
@@ -11,7 +11,33 @@ config.window_decorations = "TITLE | RESIZE"
 
 -- Color scheme
 config.color_scheme = 'rose-pine'
--- config.window_background_opacity = 0.95
+
+-- Background configuration
+config.window_background_opacity = 0.9
+config.win32_system_backdrop = "Acrylic"
+config.background = {
+  {
+    source = {
+      File = 'C:\\Users\\William\\Pictures\\wallpapers\\wallhaven-j8rj15.jpg',
+    },
+    height = "Cover",  -- This preserves aspect ratio while filling the viewport
+    width = "100%",
+    horizontal_align = "Center",
+    vertical_align = "Middle",
+  },
+  {
+    -- TODO: rose pine colors, figure out a better way 
+    source = { Color = "rgba(25, 23, 36, 0.85)" },
+    height = "100%",
+    width = "100%",
+  },
+}
+
+
+-- Enable Acrylic effect for better blending
+config.win32_system_backdrop = "Acrylic"
+config.window_background_opacity = 0.95
+
 
 -- Font configuration
 config.font = wezterm.font('DroidSansM Nerd Font Propo')
