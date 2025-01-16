322227309b542963c1eb5f7e78e27ab06c6c6358
williycole <64283810+williycole@users.noreply.github.com>
2025-01-16 14:35:12 -0600

    added lualine and better theme settings

---
+++ windows/nvim/lua/custom/plugins/lualine.lua
@@ -0,0 +1,30 @@
+-- https://github.com/nvim-lualine/lualine.nvim?tab=readme-ov-file
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('lualine').setup {
+      options = {
+        theme = 'kanagawa',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {},
+      },
+    }
+  end,
+}
--- wsl/nvim/lua/custom/plugins/theme.lua
+++ wsl/nvim/lua/custom/plugins/theme.lua
@@ -43,13 +43,18 @@ local kanagawa = {
       overrides = function(colors)
         local theme = colors.theme
         return {
-          TelescopeTitle = { fg = theme.ui.special, bold = true },
-          TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-          TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-          TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-          TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+          NormalFloat = { bg = 'none' },
+          FloatBorder = { bg = 'none' },
+          FloatTitle = { bg = 'none' },
+          TelescopeBorder = { bg = 'none' },
+          VertSplit = { bg = 'none', fg = theme.ui.bg_m3 },
+          SignColumn = { bg = 'none' },
+          LineNr = { bg = 'none' },
+          CursorLineNr = { bg = 'none' },
+          FoldColumn = { bg = 'none' },
+
+          LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+          MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
         }
       end,
     }
@@ -60,7 +65,7 @@ local kanagawa = {
 
 -- transparent configuration
 local transparent = {
-  { 'xiyaowong/transparent.nvim' },
+  'xiyaowong/transparent.nvim',
   lazy = false,
   config = function()
     require('transparent').setup {
