9a12210bb964502cfa6fb4dca4887bec823f3d1b
cboren <64283810+williycole@users.noreply.github.com>
2025-06-04 10:54:09 -0500

    added some new plugins, cleaned up dashboard and zen defaults

--- wsl/nvim/lua/plugins/default_overrides.lua
+++ wsl/nvim/lua/plugins/default_overrides.lua
@@ -1,15 +1,11 @@
 -- all overrides for LazyVim Defaults
 return {
-  -- inlay_hints
   {
-    "neovim/nvim-lspconfig",
+    "nvim-lspconfig",
     opts = {
-      inlay_hints = {
-        enabled = false,
-      },
+      inlay_hints = { enabled = false },
     },
   },
-  -- snacks
   {
     "folke/snacks.nvim",
     opts = {
@@ -19,22 +15,11 @@ return {
           icon_sources = { "nerd_fonts", "emoji" }, -- Enable both Nerd Fonts and emojis
         },
       },
-      --     zen = {
-      --       toggles = {
-      --         dim = false,
-      --         wrap = true,
-      --       },
-      --       win = {
-      --         backdrop = {
-      --           transparent = false,
-      --           -- bg = "#1a1b26", -- Custom background color
-      --           bg = (function()
-      --             local bg = vim.api.nvim_get_hl(0, { name = "Normal" }).bg
-      --             return bg and string.format("#%06x", bg) or "#1a1b26"
-      --           end)(),
-      --         },
-      --       },
-      --     }
+      zen = {
+        toggles = { dim = false, wrap = true },
+        win = { backdrop = { transparent = false, bg = "#1f1f28" } },
+        -- show = { statusline = true, tabline = true },
+      },
     },
     keys = {
       {
