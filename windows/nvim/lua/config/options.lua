f8c08c2bff064476290b3779230a473858726ec6
cboren <64283810+williycole@users.noreply.github.com>
2025-09-06 13:26:59 -0500

    same

--- windows/nvim/lua/config/options.lua
+++ windows/nvim/lua/config/options.lua
@@ -1,6 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
 -- Set global defaults for TypeScript (4 spaces)
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
